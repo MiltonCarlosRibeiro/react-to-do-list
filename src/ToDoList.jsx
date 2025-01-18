778a0e1df5d64f66b94b513a197914e0cba26b3c
Milton Carlos Ribeiro <147319237+MiltonCarlosRibeiro@users.noreply.github.com>
2025-01-19 05:39:13 +0900

    Initial commit react vite project

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,10 +3,12 @@ import {useState, useEffect} from 'react'
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState(["Teste", "Comer", "Andar"])
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
+
+        setNewTask(event.target.value);
 
     }
 
@@ -30,6 +32,47 @@ function ToDoList() {
     return (
         <div className="to-do-list">
             <h1>To Do List</h1>
+
+            <div>
+                <input
+                    type="text"
+                    placeholder={"Enter a task..."}
+                    value={newTask}
+                    onChange={handleInputChange}
+                />
+                <button
+                    className="add-button"
+                    onClick={addTask}>
+                    Add Task
+                </button>
+            </div>
+
+            <ol>
+                {tasks.map((task, index) => (
+
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button
+                            className="delete-button"
+                            onClick={() => deleteTask(index)}>
+                            Delete
+                        </button>
+                        <button
+                            className="move-button"
+                            onClick={() => moveTaskUp(index)}>
+                            UP
+                        </button>
+                        <button
+                            className="move-button"
+                            onClick={() => moveTaskDown(index)}>
+                            DOWN
+                        </button>
+
+                    </li>
+                ))}
+
+            </ol>
+
         </div>
     )
 
